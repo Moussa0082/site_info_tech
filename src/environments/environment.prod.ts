7ba66f3f04bdf2685a731e041bf82645323ff912
Abdoul03 <samabdoul03@gmail.com>
2026-03-25 12:57:17 +0000

    changement de l'url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-   apiUrl: 'http://localhost:9006/api-infotech',
+   apiUrl: 'https://tuwindi.app/api/info-tech/api-infotech',
   // apiUrl1: 'https://tuwindi.app:8443/nepal'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:9006/api-infotech',
+  apiUrl: 'https://tuwindi.app/api/info-tech/api-infotech',
   // apiUrl1: 'https://tuwindi.app:8443/nepal'
 };
 
